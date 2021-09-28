7e898bc33a9146ef51484e77ea21e750154cb3c6
parseb <arsenebp@gmail.com>
2021-09-29 00:07:14 +0300

    bad

--- client/src/App.js
+++ client/src/App.js
@@ -69,10 +69,10 @@ class App extends Component {
   getCurrentGame = async () => {
     const { accounts, contract, web3js } = this.state;
     const currentgame = await this.state.contract.methods.checkAndReturnCurrentGame().call();
-    // let color= this.state.currentGame[1] == this.state.accounts[0] 
-    // if (color) { this.setState({ color:"black" })}
-    let playcolor = (currentgame.Player2Address == accounts[0]) ? ("white") : ("black") 
-    this.setState({ currentGame: currentgame, currentGameBoard: currentgame.currentGameBoard, color: playcolor, g_state: currentgame.gState });
+    let color= this.state.currentGame[1] == accounts[0] 
+    if (color) { this.setState({ color:"white" })}
+    //let playcolor = (currentgame.Player2Address == accounts[0]) ? ("white") : ("black") 
+    this.setState({ currentGame: currentgame, currentGameBoard: currentgame.currentGameBoard, color: color, g_state: currentgame.gState });
     console.log("this is current game")
     console.log(currentgame, currentgame.currentGameBoard, this.state.currentGameBoard); 
 
